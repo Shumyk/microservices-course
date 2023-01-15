af42df3dcb10f2c85a5836b0bea52b43d3a974cb
Shumyk <dima.shumyk@gmail.com>
2023-01-15 16:17:41 +0200

    initiating microservices repo with geteway, discrovery service, users and account management services

---
+++ api-account/src/test/java/rocks/shumyk/photoapp/apiaccount/ApiAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.shumyk.photoapp.apiaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
