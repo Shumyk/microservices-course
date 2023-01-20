8fae6ca4fe1b9145c11447bbacbf470e257d58ad
Shumyk <dima.shumyk@gmail.com>
2023-01-20 20:13:24 +0200

    api-users: showing port which application is using in the users endpoint

--- api-users/src/main/java/rocks/shumyk/photoappapiusers/ui/controller/UsersController.java
+++ api-users/src/main/java/rocks/shumyk/photoappapiusers/ui/controller/UsersController.java
@@ -1,15 +1,20 @@
 package rocks.shumyk.photoappapiusers.ui.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UsersController {
 
+	private final Environment env;
+
 	@GetMapping("/status/check")
 	public String status() {
-		return "Working";
+		return "Working on port: " + env.getProperty("local.server.port");
 	}
 }
