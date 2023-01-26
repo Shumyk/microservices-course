8a5e9c0d28086134118b6ce44fa2d4a884af77ed
Shumyk <dima.shumyk@gmail.com>
2023-01-26 19:37:17 +0200

    api-gateway: implementation AuthorizationHeaderFilter.apply

--- api-gateway/src/main/java/rocks/shumyk/photoapp/api/gateway/AuthorizationHeaderFilter.java
+++ api-gateway/src/main/java/rocks/shumyk/photoapp/api/gateway/AuthorizationHeaderFilter.java
@@ -1,15 +1,37 @@
 package rocks.shumyk.photoapp.api.gateway;
 
+import com.google.common.net.HttpHeaders;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
 
     @Override
     public GatewayFilter apply(final Config config) {
-        return null;
+        return (exchange, chain) -> {
+            final ServerHttpRequest request = exchange.getRequest();
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return error(exchange, "No Authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            final String authHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            final String jwt = authHeader.replace("Bearer ", "");
+
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> error(final ServerWebExchange exchange, final String errorMessage, final HttpStatus status) {
+        final ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(status);
+        return response.setComplete();
     }
 
 
