521de8836c8b77a7e83b91fef4651fad98002ff8
Shumyk <dima.shumyk@gmail.com>
2023-01-26 17:59:26 +0200

    api-users: added missing dependency; refactoring

--- api-users/src/main/java/rocks/shumyk/photo/app/api/users/security/AuthenticationFilter.java
+++ api-users/src/main/java/rocks/shumyk/photo/app/api/users/security/AuthenticationFilter.java
@@ -29,9 +29,9 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final ObjectMapper mapper;
     private final UsersService usersService;
 
-    @Value("jwt.token.expiration.time.ms")
+    @Value("${jwt.token.expiration.time.ms}")
     private long jwtTokenExpirationTime;
-    @Value("jwt.token.secret")
+    @Value("${jwt.token.secret}")
     private String jwtTokenSecret;
 
     public AuthenticationFilter(final AuthenticationManager authenticationManager,
@@ -60,18 +60,19 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     protected void successfulAuthentication(final HttpServletRequest request,
                                             final HttpServletResponse response,
                                             final FilterChain chain,
-                                            final Authentication authResult) throws IOException, ServletException {
+                                            final Authentication authResult) {
         final String username = ((User) authResult.getPrincipal()).getUsername();
         final UserDTO userDetails = usersService.getUserDetailsByEmail(username);
+        final String userId = Long.toString(userDetails.getId());
 
         final String token = Jwts.builder()
-                .setSubject(Long.toString(userDetails.getId()))
+                .setSubject(userId)
                 .setExpiration(expirationTime())
                 .signWith(SignatureAlgorithm.HS512, jwtTokenSecret)
                 .compact();
 
         response.addHeader("token", token);
-        response.addHeader("userId", Long.toString(userDetails.getId()));
+        response.addHeader("userId", userId);
     }
 
     private Date expirationTime() {
