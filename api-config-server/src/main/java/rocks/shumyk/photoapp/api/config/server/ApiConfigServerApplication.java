89b0e5e7acd1dae9fbb5a807f7e91146d760e340
Shumyk <dima.shumyk@gmail.com>
2023-01-28 13:04:12 +0200

    api-config-server: init

---
+++ api-config-server/src/main/java/rocks/shumyk/photoapp/api/config/server/ApiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package rocks.shumyk.photoapp.api.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConfigServerApplication.class, args);
+	}
+
+}
