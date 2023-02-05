c4619ac03812faabe600c2f6ada2f256d019e5d8
Shumyk <dima.shumyk@gmail.com>
2023-02-05 21:08:49 +0200

    api-users: feign support

--- api-users/src/main/java/rocks/shumyk/photo/app/api/users/config/CommonConfiguration.java
+++ api-users/src/main/java/rocks/shumyk/photo/app/api/users/config/CommonConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class ObjectsConfiguration {
+public class CommonConfiguration {
 
     @Bean
     @LoadBalanced
