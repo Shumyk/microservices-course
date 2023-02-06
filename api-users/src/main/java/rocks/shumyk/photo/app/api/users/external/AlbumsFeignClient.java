8478ea621d825f59f47ac4fa3eba71a3a7f03f74
Shumyk <dima.shumyk@gmail.com>
2023-02-06 16:58:39 +0200

    api-users: added fallback factory for feign client

--- api-users/src/main/java/rocks/shumyk/photo/app/api/users/external/AlbumsFeignClient.java
+++ api-users/src/main/java/rocks/shumyk/photo/app/api/users/external/AlbumsFeignClient.java
@@ -1,30 +1,35 @@
 package rocks.shumyk.photo.app.api.users.external;
 
+import feign.FeignException;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import rocks.shumyk.photo.app.api.users.shared.AlbumDTO;
 
-@FeignClient(value = "albums-ws", fallback = AlbumsFeignClient.AlbumsFallback.class)
+@FeignClient(value = "albums-ws", fallbackFactory = AlbumsFeignClient.AlbumsFallbackFactory.class)
 public interface AlbumsFeignClient {
     @GetMapping("/users/{userId}/albums")
     List<AlbumDTO> getAlbums(@PathVariable long userId);
 
+    @Slf4j
     @Component
-    class AlbumsFallback implements AlbumsFeignClient {
+    class AlbumsFallbackFactory implements FallbackFactory<AlbumsFeignClient> {
         @Override
-        public List<AlbumDTO> getAlbums(final long userId) {
-            return List.of(
-                    AlbumDTO.builder()
-                            .id("0")
-                            .name("Default Album")
-                            .albumId("album0")
-                            .description("Default Album returned from Circuit Breaker")
-                            .userId(String.valueOf(userId))
-                            .build()
-            );
+        public AlbumsFeignClient create(final Throwable cause) {
+            logException(cause);
+            return x -> List.of();
+        }
+
+        private void logException(final Throwable cause) {
+            if (cause instanceof FeignException fe && fe.status() == HttpStatus.NOT_FOUND.value()) {
+                log.error("404 error took place when getAlbums was called. Error message: {}", fe.getMessage(), fe);
+            } else
+                log.error("Other error took place: {}", cause.getMessage(), cause);
         }
     }
 }
