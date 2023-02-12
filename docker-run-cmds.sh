46cf847ff8838bdb7ab41da960c2ee3f56dc8ddb
Shumyk <dima.shumyk@gmail.com>
2023-02-12 20:43:51 +0200

    api-gateway: containerized properties; Dockerfile; command to run in docker

--- docker-run-cmds.sh
+++ docker-run-cmds.sh
@@ -1,5 +1,8 @@
 # command to run Config Server in container
-docker run -d -p 8012:8012 -e 'spring.rabbitmq.host=172.17.0.2' -e 'spring.profiles.active=git' shumyk/photoapp-config-server
+docker run -d -p 8012:8012 -e 'spring.rabbitmq.host=172.17.0.2' shumyk/photoapp-config-server
 
 # command to run Discover Service (Eureka) in container
-docker run -p 8010:8010 -e CONFIG_SERVER_URL=http://172.31.39.206:8012 shumyk/photoapp-discovery-service
+docker run -d -p 8010:8010 -e CONFIG_SERVER_URL='http://172.31.39.206:8012' shumyk/photoapp-discovery-service
+
+# command to run API-Gateway in container
+docker run -d -p 8082:8082 -e CONFIG_SERVER_URL='http://172.31.39.206:8012' shumyk/photoapp-api-gateway
