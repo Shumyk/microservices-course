1174eea26732b22fb8774075514993d9a9b2ee64
Shumyk <dima.shumyk@gmail.com>
2023-02-13 11:25:34 +0200

    elasticsearch: docker run command

--- docker-run-cmds.sh
+++ docker-run-cmds.sh
@@ -6,3 +6,6 @@ docker run -d -p 8010:8010 -e CONFIG_SERVER_URL='http://172.31.39.206:8012' shum
 
 # command to run API-Gateway in container
 docker run -d -p 8082:8082 -e CONFIG_SERVER_URL='http://172.31.39.206:8012' shumyk/photoapp-api-gateway
+
+# command to run ElasticSearch in container
+docker run -d -v esdata1:/usr/share/elasticsearch/data --name elasticsearch -p 9200:9200 -p 9300:9300 -e 'discovery.type=single-node' elasticsearch:8.6.1
